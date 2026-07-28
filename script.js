09b34171a1d1aa6d6b16bc2fc37f775505fe9c0e
auliansyahaidil-spec <auliansyahaidil@gmail.com>
2026-07-27 20:45:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-text
+body{
+    margin:0;
+    background:#87CEEB;
+    text-align:center;
+    font-family:Arial;
+}
+
+h1{
+    color:white;
+}
+
+canvas{
+    background:#70C5CE;
+    border:4px solid white;
+    margin-top:10px;
+}
+
+p{
+    color:white;
+    font-size:20px;
+}
