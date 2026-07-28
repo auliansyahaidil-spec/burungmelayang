f82dad1cce8b3ab4c167dd3ac38ce499654eafa1
auliansyahaidil-spec <auliansyahaidil@gmail.com>
2026-07-27 20:56:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,73 @@
 body{
-    margin:0;
-    background:#87CEEB;
-    text-align:center;
-    font-family:Arial;
+
+margin:0;
+
+padding:0;
+
+background:#6EC6FF;
+
+font-family:Arial;
+
+text-align:center;
+
+overflow:hidden;
+
 }
 
-h1{
-    color:white;
+#menu{
+
+margin-top:80px;
+
+}
+
+.logo{
+
+width:180px;
+
+}
+
+button{
+
+width:220px;
+
+height:50px;
+
+margin:10px;
+
+font-size:20px;
+
+border:none;
+
+border-radius:15px;
+
+background:#00AA44;
+
+color:white;
+
+cursor:pointer;
+
+}
+
+button:hover{
+
+background:#008833;
+
+}
+
+.hidden{
+
+display:none;
+
 }
 
 canvas{
-    background:#70C5CE;
-    border:4px solid white;
-    margin-top:10px;
-}
 
-p{
-    color:white;
-    font-size:20px;
+display:none;
+
+margin:auto;
+
+background:#87CEEB;
+
+border:5px solid white;
+
 }
